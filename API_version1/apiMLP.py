1042890d34ca8046cfef98a87b78f1e7d2864253
Gustavo Rojas Peña <2223010469@untels.edu.pe>
2025-05-04 16:38:10 -0500

    API_version1 actualizado

--- API_version1/apiMLP.py
+++ API_version1/apiMLP.py
@@ -4,12 +4,12 @@ import numpy as np
 
 app = Flask(__name__)
 
-# Cargar el modelo
-with open("C:/Users/keyla/Jupyter/ProyectosG/TP3/ModeloMLP.sav", "rb") as f: #CAMBIAR RUTA
+#Cargamos el modelo
+with open("C:/Users/keyla/Jupyter/Prediccion_Cardiopatia/API_version1/ModeloMLP.sav", "rb") as f: #CAMBIAR RUTA
     model = pickle.load(f)
 
-# Cargar el scaler
-with open("C:/Users/keyla/Jupyter/ProyectosG/TP3/scaler.sav", "rb") as f: #CAMBIAR RUTA
+#Cargamos el scaler
+with open("C:/Users/keyla/Jupyter/Prediccion_Cardiopatia/API_version1/scaler.sav", "rb") as f: #CAMBIAR RUTA
     scaler = pickle.load(f)
 
 @app.route('/')
@@ -19,7 +19,7 @@ def home():
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
     if request.method == 'POST':
-        # Obtener datos del formulario
+        #Obtenemos datos del formulario
         Edad = int(request.form['Edad'])
         Sexo = int(request.form['Sexo'])
         Tipo_Dolor_Pecho = int(request.form['Tipo_Dolor_Pecho'])
@@ -34,26 +34,29 @@ def predict():
         NumeroVasos = int(request.form['NumeroVasos'])
         Prueba_Talio = int(request.form['Prueba_Tálio'])
 
-        # Crear array de entrada
+        #Creamos array de entrada
         input_data = np.array([[Edad, Sexo, Tipo_Dolor_Pecho, Presion_Arterial, Colesterol,
                                 Nivel_Azucar_Ayunas, Res_Electrocardiografico, Frecuencia_Cardiaca,
                                 Angina_Inducida, DepresionST, DepresionST_Pendiente,
                                 NumeroVasos, Prueba_Talio]])
 
-        # Escalar los datos antes de predecir
+        #Escalamos los datos antes de predecir
         input_scaled = scaler.transform(input_data)
 
-        # Realizar la predicción
+        #Realizamos la predicción
         prediccion_mlp = model.predict(input_scaled)
 
         probabilidad_clase_1 = float(prediccion_mlp[0][0])
-        probabilidad_clase_0 = 1 - probabilidad_clase_1
-        probabilidades = [probabilidad_clase_0, probabilidad_clase_1]
-
         prediccion_binaria = int(probabilidad_clase_1 > 0.5)
 
-        prediction_text = f"Predicción para el ejemplo:\n[{prediccion_binaria}]\n"
-        prediction_text += f"Probabilidades para las clases (0 y 1):\n{probabilidades}"
+        if prediccion_binaria == 1:
+            resultado = "Positivo"
+            probabilidad = round(probabilidad_clase_1 * 100, 2)
+        else:
+            resultado = "Negativo"
+            probabilidad = round((1 - probabilidad_clase_1) * 100, 2)
+
+        prediction_text = f"Resultado del diagnóstico: {resultado} ({probabilidad}%)"
 
         return render_template('index.html', prediction_text=prediction_text)
 
